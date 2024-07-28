062c0fa3c723787ed3661bae449face5443eb792
joseignaciostassi <joseignaciostassi@gmail.com>
2024-07-28 19:06:28 -0300

    Component locality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LocalidadComponent } from './localidad/localidad.component.js';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LocalidadComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
