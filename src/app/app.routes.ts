afc6c9847ba2783abd99bf2452133205f669eefc
joseignaciostassi <joseignaciostassi@gmail.com>
2024-08-06 15:26:58 -0300

    Component navbar, localities routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { LocalidadComponent } from './core/localidad/localidad.component.js';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path:'api/localidades', component: LocalidadComponent }
+];
