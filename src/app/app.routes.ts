64bdd0399d21324bbf65fcf366e749b250727a4d
joseignaciostassi <joseignaciostassi@gmail.com>
2024-08-06 15:51:37 -0300

    Componente vehiculo agregado (con mal display del tipoVehiculo)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LocalidadComponent } from './core/localidad/localidad.component.js';
+import { VehiculosComponent } from './core/vehiculos/vehiculos.component.js';
 
 export const routes: Routes = [
-  { path:'api/localidades', component: LocalidadComponent }
+  { path:'api/localidades', component: LocalidadComponent },
+  { path:'api/vehiculos', component: VehiculosComponent }
 ];
