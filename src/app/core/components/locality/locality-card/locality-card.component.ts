4890988f3c58c9851141f3f6700bd51f0009ea67
joseignaciostassi <joseignaciostassi@gmail.com>
2024-09-17 10:41:14 -0300

    Fixed some CSS of locality

--- src/app/core/components/locality/locality-card/locality-card.component.ts
+++ src/app/core/components/locality/locality-card/locality-card.component.ts
@@ -3,11 +3,12 @@ import { ApiResponse, Locality } from '../../models/interfaces.js';
 import { LocalityService } from '../../../services/locality.service.js';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-locality-card',
   standalone: true,
-  imports: [CommonModule, MatButtonModule],
+  imports: [CommonModule, MatButtonModule, MatCardModule],
   templateUrl: './locality-card.component.html',
   styleUrl: './locality-card.component.css'
 })
