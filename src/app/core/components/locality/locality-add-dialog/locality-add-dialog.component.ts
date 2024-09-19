22b2169ff246712c65260ea48be363f9e7e79beb
joseignaciostassi <joseignaciostassi@gmail.com>
2024-09-19 16:10:49 -0300

    Modified the locality add dialog to work wth reactive forms module

--- src/app/core/components/locality/locality-add-dialog/locality-add-dialog.component.ts
+++ src/app/core/components/locality/locality-add-dialog/locality-add-dialog.component.ts
@@ -1,38 +1,43 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialogRef,MatDialogModule } from '@angular/material/dialog';
+import { MatDialogRef, MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-locality-add-dialog',
   standalone: true,
-  imports: [MatDialogModule, MatFormFieldModule, FormsModule, MatButtonModule, MatInputModule],
+  imports: [CommonModule, MatDialogModule, MatFormFieldModule, ReactiveFormsModule, MatButtonModule, MatInputModule],
   templateUrl: './locality-add-dialog.component.html',
   styleUrl: './locality-add-dialog.component.css'
 })
-export class LocalityAddDialogComponent {
-  locality = {
-    name: '',
-    province: ''
-  };
+export class LocalityAddDialogComponent implements OnInit {
+  
+  localityForm!: FormGroup;
 
   constructor(
-    private dialogRef: MatDialogRef<LocalityAddDialogComponent>
+    private dialogRef: MatDialogRef<LocalityAddDialogComponent>,
+    private fb: FormBuilder
   ) {}
+
+  ngOnInit() {
+    this.localityForm = this.fb.group({
+    name: ['', Validators.required],
+    province: ['', Validators.required]
+  })
+  }
 
   onCancel(): void {
     this.dialogRef.close();
   }
 
   onSave(): void {
-    if (this.locality.name && this.locality.province) {
-      this.dialogRef.close(this.locality);
+    if (this.localityForm.valid) {
+      this.dialogRef.close(this.localityForm.value);
     } else {
-      console.error('Name and province are required');
+      this.localityForm.markAllAsTouched();
     }
   }
-
 }
-
