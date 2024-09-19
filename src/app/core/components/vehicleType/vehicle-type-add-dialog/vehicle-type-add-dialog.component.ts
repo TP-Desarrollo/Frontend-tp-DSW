2562ecb9d075e9fc2653efba94f0c9d5871ebc8b
joseignaciostassi <joseignaciostassi@gmail.com>
2024-09-19 16:11:14 -0300

    Modified the vehicle type add dialog to work wth reactive forms module

--- src/app/core/components/vehicleType/vehicle-type-add-dialog/vehicle-type-add-dialog.component.ts
+++ src/app/core/components/vehicleType/vehicle-type-add-dialog/vehicle-type-add-dialog.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -9,29 +9,35 @@ import { MatInputModule } from '@angular/material/input';
 @Component({
   selector: 'app-vehicle-type-add-dialog',
   standalone: true,
-  imports: [CommonModule ,MatDialogModule, MatFormFieldModule, FormsModule, MatButtonModule, MatInputModule],
+  imports: [CommonModule, MatDialogModule, MatFormFieldModule, ReactiveFormsModule, MatButtonModule, MatInputModule],
   templateUrl: './vehicle-type-add-dialog.component.html',
   styleUrl: './vehicle-type-add-dialog.component.css'
 })
-export class VehicleTypeAddDialogComponent {
+export class VehicleTypeAddDialogComponent implements OnInit {
 
-  vehicleType = {
-    type: '',
-    description: ''
-  };
+  vehicleTypeForm!: FormGroup;
 
-  constructor(private dialogRef: MatDialogRef<VehicleTypeAddDialogComponent>) {}
+  constructor(
+    private dialogRef: MatDialogRef<VehicleTypeAddDialogComponent>,
+    private fb: FormBuilder
+  ) {}
+
+  ngOnInit() {
+    this.vehicleTypeForm = this.fb.group({
+      type: ['', Validators.required],
+      description: ['', Validators.required]
+    });
+  }
 
   onCancel(): void {
     this.dialogRef.close();
   }
 
   onSave(): void {
-    if (this.vehicleType.type && this.vehicleType.description) {
-      this.dialogRef.close(this.vehicleType);
+    if (this.vehicleTypeForm.valid) {
+      this.dialogRef.close(this.vehicleTypeForm.value);
     } else {
-      console.error('Type and description are required');
+      this.vehicleTypeForm.markAllAsTouched();
     }
   }
-
 }
