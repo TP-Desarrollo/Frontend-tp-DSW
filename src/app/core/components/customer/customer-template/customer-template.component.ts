a99aa29977c2e59eac168812d145fd55e988e94a
joseignaciostassi <joseignaciostassi@gmail.com>
2024-10-04 11:56:41 -0300

    Added customer template component

---
+++ src/app/core/components/customer/customer-template/customer-template.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-template',
+  standalone: true,
+  imports: [],
+  templateUrl: './customer-template.component.html',
+  styleUrl: './customer-template.component.css'
+})
+export class CustomerTemplateComponent {
+
+}
