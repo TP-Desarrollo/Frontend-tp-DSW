3e4e5977909655a16cc32e7e150ac55b4a04c8c4
joseignaciostassi <joseignaciostassi@gmail.com>
2024-10-04 11:57:41 -0300

    Added customer and rentals routes to app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { VehicleTemplateComponent } from './core/components/vehicles/vehicle-template/vehicle-template.component';
 import { LocalityTemplateComponent } from './core/components/locality/locality-template/locality-template.component.js';
 import { VehicleTypeTemplateComponent } from './core/components/vehicleType/vehicle-type-template/vehicle-type-template.component.js';
+import { RentTemplateComponent } from './core/components/rent/rent-template/rent-template.component.js';
+import { CustomerTemplateComponent } from './core/components/customer/customer-template/customer-template.component.js';
 
 
 export const routes: Routes = [
-  { path:'rentals', component: VehicleTemplateComponent},
-  { path:'customers', component: VehicleTemplateComponent},
+  { path:'rentals', component: RentTemplateComponent},
+  { path:'customers', component: CustomerTemplateComponent},
   { path:'employees', component: VehicleTemplateComponent},
   { path:'localities', component: LocalityTemplateComponent},
   { path:'vehicles', component: VehicleTemplateComponent},
